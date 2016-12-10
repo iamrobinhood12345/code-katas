517f7621bf792073259b34de0573374a356f408c
iamrobinhood12345 <swordsshadow@Williams-MacBook-Pro.local>
2016-12-09 23:39:53 -0800

    commting remove first last of a string kata

---
+++ test_remove_first_last.py
@@ -0,0 +1,6 @@
+Test.describe("Tests")
+Test.assert_equals(remove_char('eloquent'), 'loquen')
+Test.assert_equals(remove_char('country'), 'ountr')
+Test.assert_equals(remove_char('person'), 'erso')
+Test.assert_equals(remove_char('place'), 'lac')
+Test.assert_equals(remove_char('ok'), '')
