aae5a16ba299079816822164f7ab16b029b499a2
Allyza Marquez <allyzamarquez@Allyzas-MacBook-Pro.local>
2016-12-19 01:42:32 -0800

    implementation of proper_parenthetics and tested using doubly_linked_list for push, pop, and remove methods. no addition was used in this implementation.

---
+++ proper_parenthetics.py
@@ -0,0 +1,26 @@
+"""This function takes a unicode string (text)
+as input and returns whether the string is properly closed
+parenthetically."""
+
+from doubly_linked_list import Doubly_Linked_List
+
+def proper_parenthetics(text):
+	"""Returns a 1 if the string is parenthetically open.
+	Returns a 0 if the string is parenthetically balanced.
+	Returns a -1 if the string is parenthetically balaned."""
+	parenthetics = Doubly_Linked_List()
+	for char in text:
+		if char == '(' or char == ')':
+			parenthetics.push(char)
+	while True:
+		try:
+			a = parenthetics.pop()
+			if a != ')':
+				return 1
+			else:
+				try:
+					parenthetics.remove('(')
+				except:
+					return -1
+		except:
+			return 0
